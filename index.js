463e94b193e04993c2cfed871826fd29cd34accf
Tom Nielsen <tanielsen@gmail.com>
2025-09-06 16:35:14 +0100

    onlyViews

--- index.js
+++ index.js
@@ -4,32 +4,38 @@ module.exports = {
       script: `/plugins/public/pivottable@${
         require("./package.json").version
       }/jquery-ui.min.js`,
+      onlyViews: ["Pivot table"],
     },
     {
       css: `/plugins/public/pivottable@${
         require("./package.json").version
       }/jquery-ui.min.css`,
+      onlyViews: ["Pivot table"],
     },
     {
       script: `/plugins/public/pivottable@${
         require("./package.json").version
       }/pivot.min.js`,
+      onlyViews: ["Pivot table"],
     },
     {
       script: `/plugins/public/pivottable@${
         require("./package.json").version
       }/moment.min.js`,
+      onlyViews: ["Pivot table"],
     },
     {
       script: `/plugins/public/pivottable@${
         require("./package.json").version
       }/plotly_renderers.min.js`,
       defer: true,
+      onlyViews: ["Pivot table"],
     },
     {
       css: `/plugins/public/pivottable@${
         require("./package.json").version
       }/pivot.min.css`,
+      onlyViews: ["Pivot table"],
     },
   ],
   dependencies: ["@saltcorn/json", "@saltcorn/visualize"],
